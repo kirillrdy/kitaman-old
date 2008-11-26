3aa37e7dd0f4ab182fcdecdd8b98d02de942a420
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2008-11-26 14:30:59 +1100

    fix to install ruby libs in right place

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ mkdir -p $PREFIX/var/kitaman/{build,install}
 mkdir -p $PREFIX/usr/kitaman/{pkg,src,kita_files}
 
 cp kitaman.rb $PREFIX/usr/bin/kitaman
+mkdir -p $PREFIX/usr/lib/ruby/1.9.1/kitaman
 cp lib/* $PREFIX/usr/lib/ruby/1.9.1/kitaman/
 cp etc/kitaman.conf $PREFIX/etc/
 #cp kitaman.repos $PREFIX/etc/
