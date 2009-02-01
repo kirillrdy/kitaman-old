07965993da205ce2a22c1d1a9de1d93085144fdd
Kirill Radzikhovskyy <kirillrdy@silverpond.com.au>
2009-02-01 20:28:09 +1100

    lots and lots of fixes, but main thing is GPL3

--- lib/make.rb
+++ lib/make.rb
@@ -1,4 +1,20 @@
-# This class adds support for building make driven packages
+#    Kitaman - Software Project Manager
+#    /-Promise to a little girl and a big world-/
+#
+#    Copyright (C) 2009  Kirill Radzikhovskyy <kirillrdy@silverpond.com.au>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 require 'kitaman/kita_helper'
 require 'kitaman/kitaman_helper'
@@ -74,6 +90,7 @@ class Kita
     for file in files_list_local
       if file.index('.patch')
         file = File.basename(file)
+        puts "Patching using #{file}".style :red
         `cd #{KitamanConfig.config['BUILD_DIR']} && patch -Np1 -i #{KitamanConfig.config['SRC_DIR']}/#{file}`
       end
     end
