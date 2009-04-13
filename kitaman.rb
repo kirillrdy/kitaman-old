3faa22a4a4f212f6a5f5aaa39e93d89c312da949
Kirill Radzikhovskyy <kirillrdy@silverpond.com.au>
2009-04-13 12:38:38 +1000

    regression fix

--- kitaman.rb
+++ kitaman.rb
@@ -136,7 +136,7 @@ class Kitaman
 
     load_needed_module(kita_instance.info['NAME'])
       
-    if not @node_hash[target] and (not kita_instance.installed? or @options[:rebuild])
+    if not @node_hash[target] and (not kita_instance.installed? or @options[:rebuild] or @options[:deep])
 
       node_to_be_inserted = Tree::TreeNode.new(target,kita_instance)
             
