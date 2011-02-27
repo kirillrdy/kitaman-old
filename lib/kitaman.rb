0acf2e8ad9ce363b8600cfe1e40b90d4abd77a35
Kirill <kirillrdy@gmail.com>
2011-02-27 20:19:10 +1100

    hopefully fixed that parser again

--- lib/kitaman.rb
+++ lib/kitaman.rb
@@ -8,7 +8,7 @@ module Kitaman
   list_of_modules_to_load = ['argument_parser','package','file',
                              'shell','error','log','config',
                              'downloader','colours','computer',
-                             'user','repository','dsl/make','package_dsl']
+                             'user','files_database','repository','dsl/make','package_dsl']
 
   list_of_modules_to_load.each {|x| require_relative 'kitaman/'+ x }
 
