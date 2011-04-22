75810260f1a991f1bad0f930b8c508d2afb38462
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-04-22 13:02:02 +1000

    Updated config

--- lib/kitaman/config.rb
+++ lib/kitaman/config.rb
@@ -2,27 +2,25 @@ module Kitaman
   class Config
     PREFIX=File.dirname(__FILE__)+'/../..'
 
-    #KITA_FILES_DIR="#{PREFIX}/kita_files"
+    GLOBAL_PREFIX='/var/kitaman'
 
-    SRC_DIR="#{PREFIX}/workdir/src"
-    STATE_DIR="#{PREFIX}/workdir/state"
-    BUILD_DIR="#{PREFIX}/workdir/build"
-    PKG_DIR="#{PREFIX}/workdir/pkg"
-    FAKE_INSTALL_DIR="#{PREFIX}/workdir/install"
+    SRC_DIR="#{GLOBAL_PREFIX}/workdir/src"
+    STATE_DIR="#{GLOBAL_PREFIX}/workdir/state"
+    BUILD_DIR="#{GLOBAL_PREFIX}/workdir/build"
+    PKG_DIR="#{GLOBAL_PREFIX}/workdir/pkg"
     TEMP_DIR="/var/tmp"
-    
+
     # TODO this should be called repo
     SOURCES_LIST_LOCATION = "#{PREFIX}/conf/sources.list"
-    
-    REPOSITORIES_BASE_PATH = "#{PREFIX}/repositories"
-    
-    SRC_MARSHAL_FILE="#{PREFIX}/workdir/src.db"
+
+    REPOSITORIES_BASE_PATH = "#{GLOBAL_PREFIX}/repositories"
+
+    SRC_MARSHAL_FILE="#{GLOBAL_PREFIX}/workdir/src.db"
 
     def self.init
       Shell.execute "mkdir -p #{SRC_DIR}"
       Shell.execute "mkdir -p #{STATE_DIR}"
       Shell.execute "mkdir -p #{BUILD_DIR}"
-      Shell.execute "mkdir -p #{FAKE_INSTALL_DIR}"
       Shell.execute "mkdir -p #{PKG_DIR}"
       Shell.execute "mkdir -p #{REPOSITORIES_BASE_PATH}"
     end
