68dfafa66827293db75f4461b55a61f67b1752a5
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-04-22 13:57:30 +1000

    Added terminal module to load list

--- lib/kitaman.rb
+++ lib/kitaman.rb
@@ -8,6 +8,7 @@ module Kitaman
   list_of_modules_to_load = ['argument_parser','package','file',
                              'shell','error','log','config',
                              'downloader','colours','computer',
+                             'terminal',
                              'user','files_database','repository','dsl/make','dsl/meta','package_dsl']
 
   list_of_modules_to_load.each {|x| require_relative 'kitaman/'+ x }
